25072cbb6c1710832ca3ed4acb7a1600ba091cc6
TT-RR <kaiza1mage@gmail.com>
2023-10-04 17:14:22 +0900

    update

--- js/ideaLab.js
+++ js/ideaLab.js
@@ -1,8 +1,17 @@
+$(function () {
+    setTimeout(function () {
+        $('.start p').fadeIn(1600);
+    }, 500); //0.5秒後にロゴをフェードイン!
+    setTimeout(function () {
+        $('.start').fadeOut(500);
+    }, 2500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+});
+
 $('.slider').slick({
-    autoplay: true,//自動的に動き出すか。初期値はfalse。
-    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-    speed: 500,//スライドのスピード。初期値は300。
-    slidesToShow: 3,//スライドを画面に3枚見せる
+    autoplay: true,//自動で動く
+    infinite: true,//スライドをループさせる
+    speed: 500,
+    slidesToShow: 3,
     slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
     prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
     nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
